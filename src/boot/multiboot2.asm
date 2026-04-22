4c6e2c753fb30e856f0b3236580b3ea73c8cf0a4
technofix-syh <technofix.syh@gmail.com>
2026-04-21 23:00:29 -0400

    Update multiboot2.asm

--- src/boot/multiboot2.asm
+++ src/boot/multiboot2.asm
@@ -1,15 +1,3 @@
-; Header Multiboot2 para bootloader
-section .multiboot_header
-header_start:
-    dd 0xe85250d6                ; Magic number (Multiboot 2)
-    dd 0                         ; Architecture 0 (x86_64)
-    dd header_end - header_start ; Header length
-
-    ; Checksum
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-
-    ; Required end tag
-    dw 0    ; Type
-    dw 0    ; Flags
-    dd 8    ; Size
-header_end:
+; Este archivo puede estar vacío o contener sólo el header multiboot.
+; Lo dejamos vacío porque el header ya está en boot.asm.
+; Si tu proyecto lo requiere, puedes mantenerlo así.
