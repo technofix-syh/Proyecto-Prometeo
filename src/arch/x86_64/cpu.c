3208c3d08374a1cc679bffe5d6977f976d4ed613
technofix-syh <technofix.syh@gmail.com>
2026-04-21 23:24:39 -0400

    Refactor x86_64 CPU initialization function

--- src/arch/x86_64/cpu.c
+++ src/arch/x86_64/cpu.c
@@ -1,8 +1,4 @@
-/* Inicialización básica de la CPU x86_64 */
-#include <cpu.h>
-
-void x86_64_cpu_init(void) 
-{
-    // Por ahora, solo una función placeholder
-    // Aquí irá la inicialización de GDT, IDT, etc.
+// Funciones básicas de CPU (stub)
+void cpu_init(void) {
+    // Por ahora, nada
 }
